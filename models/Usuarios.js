21a0df7349f83822a5b04fde61a7a7147957a46d
Federico Sustaita <fedesustaitaf@gmail.com>
2026-01-07 18:56:08 -0300

    Nombre de la empresa

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const usuarioSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  idDeListado: { type: Number, default: 1 }
+  idDeListado: { type: Number, default: 1 },
+  empresa: { type: String},
 });
 // Hook para generar idDeListado automáticamente
 usuarioSchema.pre("save", async function() {
