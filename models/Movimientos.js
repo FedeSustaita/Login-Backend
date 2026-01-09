a79b4a230cc32152d0a3e0afab7c4a45aba0cbb5
Federico Sustaita <fedesustaitaf@gmail.com>
2026-01-09 15:46:47 -0300

    A

--- models/Movimientos.js
+++ models/Movimientos.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const movimientoSchema  = new mongoose.Schema({
     tipo: { type: String, required: true },
     producto: { type: String, required: true },
+    variante: { type: String, default: "" },
     cantidad: { type: Number, required: true },
     listadoId: { type: Number, required: true,},
     precio: { type: Number, required: true,},
